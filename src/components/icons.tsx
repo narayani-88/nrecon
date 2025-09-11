ba04cc30b15b4503c5a17531b4989e63c4220219
narayani pandey <narayanip868@gmail.com>
2025-09-11 11:03:08 +0000

    now look at my logo its not lloking good which you have used on website

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,17 +7,15 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10" />
-      <path d="M12 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-      <path d="M14.5 14.5a6 6 0 1 0-5-5" />
-      <path d="M12 6V3" />
-      <path d="M12 12h3" />
-      <path d="M9 15v-3" />
+      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+      <path d="M12 11.1c.5 0 .9-.4.9-.9s-.4-.9-.9-.9-.9.4-.9.9.4.9.9.9z" />
+      <path d="M14.2 14.2a4 4 0 1 0-4.4-4.4" />
+      <path d="M12 6.5V12" />
     </svg>
   );
 }
