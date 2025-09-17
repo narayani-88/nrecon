f0505ecf785eb47872ccbe2cf5fc5dc580892334
Your Name <your-email@example.com>
2025-09-17 19:52:18 +0530

    undergoing development chnages

--- babel.config.js
+++ babel.config.js
@@ -1,36 +1,22 @@
 module.exports = (api) => {
-  // This caches the Babel config
+  // Cache based on NODE_ENV
   api.cache.using(() => process.env.NODE_ENV);
-  
-  const presets = [
-    [
-      'next/babel',
-      {
-        'preset-react': {
-          runtime: 'automatic',
-          importSource: '@emotion/react',
-        },
-      },
-    ],
-  ];
-
-  const plugins = [
-    // Add any additional plugins here
-    '@babel/plugin-transform-runtime',
-  ];
 
   return {
-    presets,
-    plugins,
-    // Ignore node_modules by default (can be overridden by specific rules)
-    ignore: ['node_modules'],
-    // Only process handlebars with specific loader
-    overrides: [
-      {
-        // Use a safe function so Babel doesn't error when no filename is provided
-        test: (filename) => Boolean(filename && /[\\/]node_modules[\\/]handlebars[\\/]/.test(filename)),
-        presets: ['@babel/preset-env'],
-      },
+    presets: [
+      [
+        'next/babel',
+        {
+          'preset-react': {
+            runtime: 'automatic',
+            importSource: '@emotion/react',
+          },
+        },
+      ],
     ],
+    plugins: [
+      '@babel/plugin-transform-runtime',
+    ],
+    // Intentionally avoid any filename-dependent fields like `ignore` or `overrides`
   };
 };
